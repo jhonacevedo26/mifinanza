c3d4421ad052c5cbc0e98864ec7dc1768a574dc6
jhonacevedo26 <107655595+jhonacevedo26@users.noreply.github.com>
2026-05-19 11:11:25 -0500

    Update service-worker.js
    arreglo temporal

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,3 @@
-const CACHE_NAME = 'mifinanza-v2';
-const ASSETS = [
-  '/mifinanza/mifinanza-sheets.html',
-  '/mifinanza/manifest.json'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request)); });
